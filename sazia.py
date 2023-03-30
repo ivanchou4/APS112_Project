ae87c98be46b2553050914a5017f35ea29a8e435
sazia19 <129338355+sazia19@users.noreply.github.com>
2023-03-29 20:36:00 -0400

    Update sazia.py

--- sazia.py
+++ sazia.py
@@ -1,1 +1,17 @@
+from colorthief import ColorThief 
+import matplotlib.pyplot as plt
+from PIL import Image
 
+#Open image using Image module
+img = Image.open("Fall tree background.jpg")
+img.show()
+#Show actual Image
+
+color_thief = ColorThief("Fall tree background.jpg")
+dominant_color = color_thief.get_color(quality=1)
+plt.imshow([[dominant_color]])
+plt.show()
+
+palette = color_thief.get_palette(color_count=5)
+plt.imshow([[palette[i] for i in range(5)]])
+plt.show()
